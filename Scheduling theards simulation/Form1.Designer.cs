aa0bfe01512466db5080aca0082960ae7e903f5c
Reinhardt <miller.reinhardt123@gmail.com>
2019-05-22 14:43:58 +0200

    first commit

---
+++ Scheduling theards simulation/Form1.Designer.cs
@@ -0,0 +1,310 @@
+﻿namespace Scheduling_theards_simulation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.lblProcess = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtBur = new System.Windows.Forms.TextBox();
+            this.txtArr = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.button4 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 226);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(424, 251);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Enter no of Processes";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10"});
+            this.comboBox1.Location = new System.Drawing.Point(129, 22);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // lblProcess
+            // 
+            this.lblProcess.AutoSize = true;
+            this.lblProcess.Location = new System.Drawing.Point(6, 27);
+            this.lblProcess.Name = "lblProcess";
+            this.lblProcess.Size = new System.Drawing.Size(180, 13);
+            this.lblProcess.TabIndex = 4;
+            this.lblProcess.Text = "Enter Arrival/ Burst Time for process:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(129, 49);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.comboBox2);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.txtBur);
+            this.groupBox1.Controls.Add(this.txtArr);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.lblProcess);
+            this.groupBox1.Location = new System.Drawing.Point(290, 22);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(257, 156);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Arrival/Burst Time";
+            this.groupBox1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(169, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Priority";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"});
+            this.comboBox2.Location = new System.Drawing.Point(172, 72);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(53, 21);
+            this.comboBox2.TabIndex = 10;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(71, 127);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Next";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtBur
+            // 
+            this.txtBur.Location = new System.Drawing.Point(74, 91);
+            this.txtBur.Name = "txtBur";
+            this.txtBur.Size = new System.Drawing.Size(48, 20);
+            this.txtBur.TabIndex = 8;
+            // 
+            // txtArr
+            // 
+            this.txtArr.Location = new System.Drawing.Point(74, 55);
+            this.txtArr.Name = "txtArr";
+            this.txtArr.Size = new System.Drawing.Size(48, 20);
+            this.txtArr.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 94);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Burst Time:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 62);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Arrival Time:";
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(315, 483);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(121, 30);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Run Algorithm";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioButton2);
+            this.groupBox2.Controls.Add(this.radioButton1);
+            this.groupBox2.Location = new System.Drawing.Point(30, 94);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(234, 49);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(94, 16);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(101, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Multiple Queues";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(3, 16);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(88, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Round Robin";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.Location = new System.Drawing.Point(315, 519);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(121, 30);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Run Algorithm";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(462, 226);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(130, 251);
+            this.listBox2.TabIndex = 10;
+            this.listBox2.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(612, 597);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label lblProcess;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtBur;
+        private System.Windows.Forms.TextBox txtArr;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ListBox listBox2;
+    }
+}
+
---
+++ Scheduling theards simulation/Form1.cs
@@ -0,0 +1,341 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Scheduling_theards_simulation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int[,] array = new int[10, 4];
+        int count = 0;
+        int numOfProcesses;
+        Boolean done = false;
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        public Boolean CheckDone()
+        {
+            int tot = 0;
+            for (int i = 0; i < 9; i++)
+            {
+                tot += array[i, 1];
+            }
+            if (tot < 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public String GetProcess(int pro)
+        {
+            switch (pro)
+            {
+                case 0:
+                    return "A";
+                case 1:
+                    return "B";
+                case 2:
+                    return "C";
+                case 3:
+                    return "D";
+                case 4:
+                    return "E";
+                case 5:
+                    return "F";
+                case 6:
+                    return "G";
+                case 7:
+                    return "H";
+                case 8:
+                    return "I";
+                case 9:
+                    return "J";
+            }
+            return "a";
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int arrive = Convert.ToInt32(txtArr.Text);
+            int burst = Convert.ToInt32(txtBur.Text);
+            array[count, 0] = arrive;
+            array[count, 1] = burst;
+            array[count, 3] = 1;
+            if(comboBox2.SelectedIndex <= 0)
+            {
+                array[count, 2] = 0;
+            }
+            else
+            {
+                array[count, 2] = comboBox2.SelectedIndex + 1;
+            }
+            
+            if (count < (numOfProcesses - 1))
+            {
+                listBox1.Items.Add(GetProcess(count) + "\t" + arrive + "\t" + burst);
+                count++;
+                lblProcess.Text = "Enter Arrival/ Burst Time for process:" + GetProcess(count);
+                txtArr.Text = "";
+                txtBur.Text = "";
+                txtArr.Select();
+            }
+            else
+            {
+                button2.Enabled = false;
+                button3.Enabled = true;
+                button4.Enabled = true;
+                listBox1.Items.Add(GetProcess(count) + "\t" + arrive + "\t" + burst);
+            }
+            comboBox2.SelectedIndex = 0;
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        public int RandomNumber()
+        {
+            Random random = new Random();
+            return random.Next(1, 10);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            groupBox1.Visible = true;
+            button1.Enabled = false;
+            numOfProcesses = (comboBox1.SelectedIndex + 1);
+            listBox1.Items.Add("Process \t Arrival \t Burst");
+            lblProcess.Text = "Enter Arrival/ Burst Time for process:" + GetProcess(count);
+        }
+        private async void button3_Click(object sender, EventArgs e)
+        {
+            listBox2.Visible = true;
+            listBox2.Items.AddRange(listBox1.Items);
+            listBox1.Items.Clear();
+            int cLoop = 0;
+            
+            while (!done)
+            {
+                
+                for (int i = 0; i < numOfProcesses; i++)
+                {
+                    if (array[i, 0] <= cLoop)
+                    {
+                        if (array[i, 1] != 0)
+                        {
+                            if(array[i,2] < 2)
+                            {
+                                for(int k = 0; k <= array[i,1]; k++)
+                                {
+                                    array[i, 1] -= 1;
+                                    listBox1.Items.Add(GetProcess(i) + (array[i, 1]));
+                                    await Task.Delay(500);
+                                    cLoop++;
+                                }
+                            }
+                            else
+                            {
+                                array[i, 1] -= 1;
+                                listBox1.Items.Add(GetProcess(i) + (array[i, 1]));
+                                await Task.Delay(500);
+                                cLoop++;
+                            }
+                        }
+                        else
+                        {
+                            listBox1.Items.Add("----" + cLoop);
+                            await Task.Delay(500);
+                            cLoop++;
+                            int doneCount = 0;
+                            for (int j = 0; j < numOfProcesses; j++)
+                            {
+                                if (array[j, 1] == 0)
+                                {
+                                    doneCount += 1;
+                                }
+                            }
+                            if (doneCount == numOfProcesses)
+                            {
+                                listBox1.Items.Add("Done");
+                                done = true;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        cLoop++;
+                        int doneCount = 0;
+                        for(int j = 0; j < numOfProcesses; j++)
+                        {
+                            if(array[j,1] == 0)
+                            {
+                                doneCount += 1;
+                            }
+                        }
+                        if(doneCount == numOfProcesses)
+                        {
+                            listBox1.Items.Add("Done");
+                            done = true;
+                        }
+
+                    }
+                    if(i >= (numOfProcesses -1))
+                    {
+                        i = -1;
+                    }
+                    if(done)
+                    {
+                        i = 11;
+                    }
+                }
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            comboBox2.Visible = true;
+            label2.Visible = true;
+            button3.Visible = true;
+            button4.Visible = false;
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            comboBox2.Visible = false;
+            label2.Visible = false;
+            button4.Visible = true;
+            button3.Visible = false;
+        }
+
+        private async void button4_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            int cLoop = 0;
+            int TQ = 0;
+            while (!done)
+            {
+
+                for (int i = 0; i < numOfProcesses; i++)
+                {
+                    if (array[i, 0] <= cLoop)
+                    {
+                        if (array[i, 1] != 0)
+                        {
+                            if (array[i, 2] < 2)
+                            {
+                                for (int k = 0; k <= array[i, 1]; k++)
+                                {
+                                    array[i, 1] -= 1;
+                                    listBox1.Items.Add(GetProcess(i) + (array[i, 1]));
+                                    await Task.Delay(500);
+                                    cLoop++;
+                                }
+                            }
+                            else
+                            {
+                                if(array[i,3] == 1)
+                                {
+                                    array[i, 1] -= 1;
+                                    array[i, 3] += 1;
+                                    listBox1.Items.Add(GetProcess(i) + (array[i, 1]));
+                                    await Task.Delay(500);
+                                    cLoop++;
+                                }
+                                else
+                                {
+                                    if (array[i, 3] == 2)
+                                    {
+                                        array[i, 1] -= 2;
+                                        array[i, 3] += 1;
+                                        listBox1.Items.Add(GetProcess(i) + (array[i, 1]));
+                                        await Task.Delay(500);
+                                        cLoop++;
+                                    }
+                                    else
+                                    {
+                                        array[i, 1] -= 3;
+                                        listBox1.Items.Add(GetProcess(i) + (array[i, 1]));
+                                        await Task.Delay(500);
+                                        cLoop++;
+                                    }
+                                }
+                               
+                            }
+                        }
+                        else
+                        {
+                            listBox1.Items.Add("----" + cLoop);
+                            cLoop++;
+                            int doneCount = 0;
+                            for (int j = 0; j < numOfProcesses; j++)
+                            {
+                                if (array[j, 1] == 0)
+                                {
+                                    doneCount += 1;
+                                }
+                            }
+                            if (doneCount == numOfProcesses)
+                            {
+                                listBox1.Items.Add("Done");
+                                done = true;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        int doneCount = 0;
+                        for (int j = 0; j < numOfProcesses; j++)
+                        {
+                            if (array[j, 1] == 0)
+                            {
+                                doneCount += 1;
+                            }
+                        }
+                        if (doneCount == numOfProcesses)
+                        {
+                            listBox1.Items.Add("Done");
+                            done = true;
+                        }
+                    }
+                    if (i >= (numOfProcesses - 1))
+                    {
+                        i = -1;
+                    }
+                    if (done)
+                    {
+                        i = 11;
+                    }
+                }
+            }
+        }
+    }
+}
